78a04e3b7a42722262050e6173c99180f96a9a8a
Dovt999 <121645824+Dovt999@users.noreply.github.com>
2023-04-09 13:00:43 +0300

    Добавлен файл с кодом

---
+++ Final bs/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array, array1;
+array = new string[5]{"123", "COmplete", "lol", "Geek", "bro"};
+array1 = new string [5];
+int j = 0;
+for(int i = 0;i < 5; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array1[j] = array[i];
+        j++;
+    }
+}
+    for (int k = 0; k < j + 1; k++)
+{
+    Console.WriteLine(array1[k]);
+}
